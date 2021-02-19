7b87aa2a5e16413f8b98535629b710bdd68e29ce
VBharwani <102222589@student.swin.edu.au>
2021-02-20 01:05:19 +1100

    Adding my Day 1 and Day 2
    Adding my code of Day 1 and Day 2 excercise.
    Day 1 completed
    Day 2 In progress

---
+++ Day 2/main.py
@@ -0,0 +1,20 @@
+# amount = float(input("Amount to pay:  $"))
+# tip = float(input("How much tip do you prefer in %?: "))
+# people = float(input("How many people to divide with?:     "))
+# tip_amount = ((amount * tip)/100)/people
+# individual_cost = amount/people
+# total_per_person = tip_amount+individual_cost
+# print("Each person would pay: " + str(total_per_person))
+
+# print("----------------Day 2 Exersice----------------")
+
+# two_digit_num = input("Please enter two digit number: ")
+# first_num = int(two_digit_num[0])
+# second_num = int(two_digit_num[1])
+# print(first_num + second_num)
+
+print("----------------Day 2 BMI Calculator----------------")
+height = float(input("whats your hight in meter? "))
+weight = float(input("whats your weight in Kg's? "))
+calculatedBMI = int(weight/(height**2))
+print(calculatedBMI)
