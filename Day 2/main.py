b8bbcd71c46a5a7eb479a9f24be0d0a717f47859
VBharwani <102222589@student.swin.edu.au>
2021-02-20 01:58:18 +1100

    Day 2 Finally completed and releasing

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,25 +1,18 @@
-# amount = float(input("Amount to pay:  $"))
-# tip = float(input("How much tip do you prefer in %?: "))
-# people = float(input("How many people to divide with?:     "))
-# tip_amount = ((amount * tip)/100)/people
-# individual_cost = amount/people
-# total_per_person = tip_amount+individual_cost
-# print("Each person would pay: " + str(total_per_person))
 
-# print("----------------Day 2 Exersice----------------")
+print("----------------Day 2 Exersice----------------")
 
-# two_digit_num = input("Please enter two digit number: ")
-# first_num = int(two_digit_num[0])
-# second_num = int(two_digit_num[1])
-# print(first_num + second_num)
+two_digit_num = input("Please enter two digit number: ")
+first_num = int(two_digit_num[0])
+second_num = int(two_digit_num[1])
+print(first_num + second_num)
 
-# print("----------------Day 2 BMI Calculator----------------")
-# height = float(input("whats your hight in meter? "))
-# weight = float(input("whats your weight in Kg's? "))
-# calculatedBMI = str(int(weight/(height**2)))
-# print("Calculated BMI is " + calculatedBMI)
+print("----------------Day 2 BMI Calculator----------------")
+height = float(input("whats your hight in meter? "))
+weight = float(input("whats your weight in Kg's? "))
+calculatedBMI = str(int(weight/(height**2)))
+print("Calculated BMI is " + calculatedBMI)
 
-# print("Testing Day 2 branch in GIT")
+print("Testing Day 2 branch in GIT")
 print("-----Day 2 Life in week challenge -------")
 total_days = 90*365
 total_weeks = 90*52
@@ -34,3 +27,13 @@ weeks_to_live = total_weeks - weeks_lived
 months_to_live = total_months - months_lived
 print(
     f"You have {days_to_live} days, {weeks_to_live} weeks & {months_to_live} months left to live")
+
+print("\n-------------------Day 2 Final Tip calculator challenge----------------------\n")
+amount = float(input("Amount to pay:  $"))
+tip = float(input("How much tip do you prefer in %?: "))
+people = float(input("How many people to divide with?:     "))
+tip_amount = ((amount * tip)/100)/people
+individual_cost = amount/people
+total_per_person = tip_amount+individual_cost
+print("Each person would pay: " + str(total_per_person))
+print("\n-------------Thank you - Day 2 complete-------------")
