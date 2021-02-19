9aec2e2f9cdf644f59624ad09263493180be731c
VBharwani <102222589@student.swin.edu.au>
2021-02-20 01:12:56 +1100

    Testing Github in VS if working as expected

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-amount = 0
-amount = input("Amount to pay:  ")
-tip = 0
-tip = input("How much tip do you prefer?: ")
-people = input("Howmany people to divide with?:     ")
-tip_amount = 0
-tip_amount = (amount * tip)/100
-individual_cost = amount/people
-total_per_person = tip_amount+individual_cost
-print("Each person would pay: " + total_per_person)
+# amount = float(input("Amount to pay:  $"))
+# tip = float(input("How much tip do you prefer in %?: "))
+# people = float(input("How many people to divide with?:     "))
+# tip_amount = ((amount * tip)/100)/people
+# individual_cost = amount/people
+# total_per_person = tip_amount+individual_cost
+# print("Each person would pay: " + str(total_per_person))
+
+# print("----------------Day 2 Exersice----------------")
+
+# two_digit_num = input("Please enter two digit number: ")
+# first_num = int(two_digit_num[0])
+# second_num = int(two_digit_num[1])
+# print(first_num + second_num)
+
+print("----------------Day 2 BMI Calculator----------------")
+height = float(input("whats your hight in meter? "))
+weight = float(input("whats your weight in Kg's? "))
+calculatedBMI = int(weight/(height**2))
+print(calculatedBMI)
+
+print("--------------GITHUB testing-------------------------")
