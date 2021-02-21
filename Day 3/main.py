dbfb5ac073891256917ed79eaca4481771d42e63
VBharwani <102222589@student.swin.edu.au>
2021-02-21 23:54:20 +1100

    Day 3 Addition of odd even number check exercise

--- Day 3/main.py
+++ Day 3/main.py
@@ -3,4 +3,14 @@ height = int(input("Please enter your height in cm: "))
 if height > 120:
     print("You can Ride")
 else:
-    print("Ypu cannot ride")
+    print("You cannot ride")
+
+print("----Odd Even Exersice----")
+user_number = int(
+    input("Please enter a number to check if the number is odd or even: "))
+if user_number == 0:
+    print(f"{user_number} is neither odd nor even!")
+elif user_number % 2 == 0:
+    print(f"{user_number} is an even number")
+else:
+    print(f"{user_number} is an odd number")
