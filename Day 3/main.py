a1dff890ae241c3b37a41b13911b4937b0048335
VBharwani <102222589@student.swin.edu.au>
2021-02-21 23:40:46 +1100

    Day 3 Roller Coster Ride update

---
+++ Day 3/main.py
@@ -0,0 +1,6 @@
+print("----------------- Day 3 Roller Coster Height check----------------------")
+height = int(input("Please enter your height in cm: "))
+if height > 120:
+    print("You can Ride")
+else:
+    print("Ypu cannot ride")
