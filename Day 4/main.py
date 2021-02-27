d434aa7099882c1385b2718a45b576728cf32e7d
VBharwani <102222589@student.swin.edu.au>
2021-02-27 23:08:17 +1100

    Day 4 Heads & Tails challenge

---
+++ Day 4/main.py
@@ -0,0 +1,7 @@
+import random
+print("----------Day 4 Heads Tails Challenge-----------")
+coin_face = random.randint(0, 1)
+if coin_face == 0:
+    print("Heads")
+elif coin_face == 1:
+    print("Tails")
