5926f03d4ad22066e84a5e625db850cccc13b31b
VBharwani <102222589@student.swin.edu.au>
2021-03-02 00:00:44 +1100

    Day 6 Websites hurdle code challenge

---
+++ Day 6/Hurdle_1.py
@@ -0,0 +1,24 @@
+# for this website: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# HURDLE 1 SOLUTION
+
+# -----------From the code below remove comments and pasted into the website code editor-----------------------
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# i = 0
+# for i in range(0, 6):
+#     jump()
+#     i += 1
---
+++ Day 6/Hurdle_4.py
@@ -0,0 +1,24 @@
+# for this website: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# HURDLE 4 SOLUTION
+
+
+# -----------From the code below remove comments and pasted into the website code editor-----------------------
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+#         turn_left()
+
+# while not at_goal():
+#     jump()
