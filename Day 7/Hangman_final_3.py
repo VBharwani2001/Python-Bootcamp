e7c6963a4e6becf5d8272e3a98ab96b4a06abc0e
VBharwani <102222589@student.swin.edu.au>
2021-03-04 00:03:12 +1100

    Day 7 Final code updated for better UX

--- Day 7/Hangman_final_3.py
+++ Day 7/Hangman_final_3.py
@@ -1,62 +1,10 @@
 import random
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_logo
+import hangman_word_list
 
-word_list = ["aardvark", "baboon", "camel"]
+stages = hangman_logo.stages
+print(hangman_logo.logo)
+word_list = hangman_word_list.word_list
 display = []
 
 # TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
@@ -64,7 +12,7 @@ display = []
 
 
 lives = len(stages) - 1
-chosen_word = word_list[random.randint(0, 2)]
+chosen_word = word_list[random.randint(0, len(word_list))]
 for _ in chosen_word:
     display.append("_")
 print(chosen_word)
@@ -73,10 +21,13 @@ word_guessed = True
 
 while word_guessed:
     guess = input("Guess the letter:   \n ").lower()
+    if guess in display:
+        print(f"You have already guessed the letter {guess}")
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = guess
+            print(display)
 # TODO-2: - If guess is not a letter in the chosen_word,
 # Then reduce 'lives' by 1.
 # If lives goes down to 0 then the game should stop and it should print "You lose."
@@ -84,10 +35,14 @@ while word_guessed:
     if guess not in chosen_word:
         lives -= 1
         print(stages[lives])
+        print(display)
     if lives == 0:
         print("You lose!")
         word_guessed = False
-    print(display)
+        print(
+            f"{'' .join(display)} is your guessed answer but the actual answer is {chosen_word}")
+
     if not "_" in display:
         word_guessed = False
         print("you win")
+        print(f"{''.join(display)} is the correct word guessed")
